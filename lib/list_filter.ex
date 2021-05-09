05cb89ba6900b045d67c1d7812469039881778fd
vinolivae <vinicius.anjos@stone.com.br>
2021-05-08 21:09:29 -0300

    feat: filter and return how many odd numbers there are

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,18 +1,31 @@
 defmodule ListFilter do
   @moduledoc """
-  Documentation for `ListFilter`.
+  filtering odd numbers by list
   """
 
   @doc """
-  Hello world.
+  call functions to filter list
 
   ## Examples
-
-      iex> ListFilter.hello()
-      :world
-
+  iex> ListFilter.call(["1", "3", "6", "43", "bike", "6", "abc"])
+  3
   """
-  def hello do
-    :world
+  @spec call([any]) :: number
+  def call(list), do: list |> filter_odd_numbers() |> count_odd_numbers()
+
+  defp filter_odd_numbers(list) do
+    list
+    |> Enum.flat_map(fn string ->
+      case Integer.parse(string) do
+        {int, _resp} -> [int]
+        :error -> []
+      end
+    end)
+    |> Enum.filter(fn number -> rem(number, 2) != 0 end)
+  end
+
+  defp count_odd_numbers(list) do
+    list
+    |> Enum.count()
   end
 end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -2,7 +2,23 @@ defmodule ListFilterTest do
   use ExUnit.Case
   doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "return a list of odd numbers" do
+      list = ["1", "3", "6", "43", "bike", "6", "abc"]
+
+      response = ListFilter.call(list)
+      expected_response = 3
+
+      assert expected_response == response
+    end
+
+    test "return a empty list" do
+      list = ["hello", "world"]
+
+      response = ListFilter.call(list)
+      expected_response = 0
+
+      assert expected_response == response
+    end
   end
 end
